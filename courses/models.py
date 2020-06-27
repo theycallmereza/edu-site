2e75cbd9073a363972e39d6d22628e55d467aa1c
Reza Heydari <mrreza1394@gmail.com>
2020-06-27 17:21:56 +0430

    add enrollment course ability

--- courses/models.py
+++ courses/models.py
@@ -25,6 +25,7 @@ class Course(models.Model):
     slug = models.SlugField(max_length=255, unique=True)
     overview = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
+    students = models.ManyToManyField(User, related_name='courses_joined', blank=True)
 
     class Meta:
         ordering = ['-created']
--- courses/views.py
+++ courses/views.py
@@ -14,6 +14,8 @@ from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
 from .models import Course, Module, Content, Subject
 from .forms import ModuleFormSet
 
+from students.forms import CourseEnrollForm
+
 
 class OwnerMixin(object):
     def get_queryset(self):
@@ -189,3 +191,8 @@ class CourseListView(TemplateResponseMixin, View):
 class CourseDetailView(DetailView):
     model = Course
     template_name = 'courses/course/detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['enroll_form'] = CourseEnrollForm(initial={'course':self.object})
+        return context
